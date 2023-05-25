aa2342dd007f25b784037719c7e0a75ffd8db026
root <root@DESKTOP-96GB21B>
2023-05-25 20:21:10 +0700

    add database

--- jurnal.sql
+++ jurnal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 11, 2023 at 03:45 PM
+-- Generation Time: May 25, 2023 at 03:20 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -24,6 +24,40 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `golongan`
+--
+
+CREATE TABLE `golongan` (
+  `id_golongan` int(11) NOT NULL,
+  `nama_golongan` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `golongan`
+--
+
+INSERT INTO `golongan` (`id_golongan`, `nama_golongan`) VALUES
+(1, 'I A / Juru Muda'),
+(2, 'I B / Juru Muda Tingkat 1'),
+(3, 'I C / Juru'),
+(4, 'I D / Juru Tingkat 1'),
+(5, 'II A / Pengatur Muda'),
+(6, 'II B / Pengatur Muda Tingkat 1'),
+(7, 'II C / Pengatur'),
+(8, 'II D / Pengatur Tingkat 1'),
+(9, 'III A / Penata Muda'),
+(10, 'III B / Penata Muda Tingkat 1'),
+(11, 'III C / Penata'),
+(12, 'III D / Penata Tingkat 1'),
+(13, 'IV A / Pembina'),
+(14, 'IV B / Pembina Tingkat 1'),
+(15, 'IV C / Pembina Utama Muda'),
+(16, '\r\nIV D / Pembina Utama Madya'),
+(17, 'IV E / Pembina Utama');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `jurnal`
 --
 
@@ -39,13 +73,6 @@ CREATE TABLE `jurnal` (
   `status` enum('terverifikasi','pending') NOT NULL,
   `id_users` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `jurnal`
---
-
-INSERT INTO `jurnal` (`id_jurnal`, `nama`, `tanggal`, `tempat`, `penyelenggara`, `foto`, `jam_mulai`, `jam_berakhir`, `status`, `id_users`) VALUES
-(3, 'Input data laporan', '2023-01-01', 'Aula Lantai 2', 'Pemerintah Kabupaten Tanggamus', '1683804579_65c36d5c8e3744ef0bd4.jpg', '00:01:00', '00:01:00', 'terverifikasi', 2);
 
 -- --------------------------------------------------------
 
@@ -96,7 +123,8 @@ CREATE TABLE `pegawai` (
 --
 
 INSERT INTO `pegawai` (`id_pegawai`, `nama_pegawai`, `nip`, `jabatan`, `tmt_jabatan`, `golongan`, `kerja_thn`, `kerja_bln`, `tmt_golongan`, `pendidikan`, `instansi_pendidikan`, `thn_lulus`) VALUES
-(3, 'Muhammad Dzaky', '123231312321', 'Pengelola Keuangan', '2023-02-01', 'V/a', 2, 4, '2024-04-01', 'STRATA III', 'Universitas Indonesia', '2024');
+(3, 'Muhammad Dzaky', '123231312321', 'Pengelola Keuangan', '2023-02-01', 'V/a', 2, 4, '2024-04-01', 'STRATA III', 'Universitas Indonesia', '2024'),
+(6, 'Muhammad Dzaky', '12312321312', 'Pengelola Keuangan', '2024-02-01', '14', 18, 1, '2023-01-01', 'DIPLOMA II', 'Universitas Indonesia', '2020');
 
 -- --------------------------------------------------------
 
@@ -109,22 +137,33 @@ CREATE TABLE `users` (
   `name` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `role` enum('user','admin','pimpinan','') NOT NULL
+  `role` enum('user','admin','pimpinan','') NOT NULL,
+  `nip` int(11) NOT NULL,
+  `golongan` int(11) NOT NULL,
+  `jabatan` varchar(255) NOT NULL,
+  `unit` varchar(255) NOT NULL,
+  `kepala` varchar(255) NOT NULL,
+  `picture` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id_users`, `name`, `username`, `password`, `role`) VALUES
-(2, 'Muhammad Dzaky', 'admin', '$2y$10$4UDQyhiz801cUw50KR008uHU2Cgu05OyTF8w7AzggtzGF4kKXrwy2', 'admin'),
-(3, 'Staff Skretariat', 'user', '$2y$10$YcxItNHg/b.xcZ6G/38sWeIqEc.LnlWnBJGiF68oL66Mw/KyJvLK2', 'user'),
-(4, 'Kasubag Umum', 'pimpinan', '$2y$10$PqQXLr7cjChaEK6L.YAykuA/0m561ZCGcSr1cnXGqMEXfLEUlYaAu', 'pimpinan'),
-(6, 'Staff Kominfo', 'user1', '$2y$10$7e3t.j2L0IR9RhR7sdYuS.b92rakk.JfpEfD8kMp3le8Q3AE9UihS', 'user');
+INSERT INTO `users` (`id_users`, `name`, `username`, `password`, `role`, `nip`, `golongan`, `jabatan`, `unit`, `kepala`, `picture`) VALUES
+(2, 'Muhammad Dzaky', 'admin', '$2y$10$4UDQyhiz801cUw50KR008uHU2Cgu05OyTF8w7AzggtzGF4kKXrwy2', 'admin', 111, 2, 'Direktur', 'Kominfo', 'Ir. Budi, S.Ag.', '1685009042_5ae5366a5f1efa252bde.jpg'),
+(3, 'Muhammad Dzaky', 'user', '$2y$10$YcxItNHg/b.xcZ6G/38sWeIqEc.LnlWnBJGiF68oL66Mw/KyJvLK2', 'user', 999, 2, 'Staff', 'Kominfo', 'Ir. Budi, S.Ag.', 'default.jpg'),
+(4, 'Kasubag Umum', 'pimpinan', '$2y$10$PqQXLr7cjChaEK6L.YAykuA/0m561ZCGcSr1cnXGqMEXfLEUlYaAu', 'pimpinan', 222, 3, 'Pimpinan', 'Kominfo', 'Dra. Siti, S.Ag.', 'default.jpg');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `golongan`
+--
+ALTER TABLE `golongan`
+  ADD PRIMARY KEY (`id_golongan`);
 
 --
 -- Indexes for table `jurnal`
@@ -155,10 +194,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `golongan`
+--
+ALTER TABLE `golongan`
+  MODIFY `id_golongan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
 -- AUTO_INCREMENT for table `jurnal`
 --
 ALTER TABLE `jurnal`
-  MODIFY `id_jurnal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_jurnal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `kinerja`
@@ -170,13 +215,13 @@ ALTER TABLE `kinerja`
 -- AUTO_INCREMENT for table `pegawai`
 --
 ALTER TABLE `pegawai`
-  MODIFY `id_pegawai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_pegawai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_users` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_users` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
